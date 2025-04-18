e83675b570a8aa245dda6890b489847cc0ab361c
Kareem Mohamed <61213045+KareemZafan@users.noreply.github.com>
2025-04-18 17:01:28 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,5 @@
 
 import pytest 
-# This is a python file 
-
-
 
  print("Hello My Team")
  
@@ -11,3 +8,11 @@ import pytest
  
  def test_add_functionality():
  	assert add(4,6) == 10
+
+# It's a change from Nada's machine
+
+def sub(a , b):
+  return a - b
+
+def test_sub_functionality():
+  assert sub(5,20) == -15
